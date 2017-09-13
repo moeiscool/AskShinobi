d7553f35340ec4fb7340fc4cc9bdc48b411ec6e9
Moe <github@m03.ca>
2017-09-13 12:20:04 -0700

    change msg

--- chatbot.js
+++ chatbot.js
@@ -59,11 +59,11 @@ client.on('message', msg => {
 
             }).end();
         break;
-        case msg.search('<@264820556375916546>'): case msg.search('@moeiscool'):
+        case !msg.search('://')&&msg.search('?')&&(msg.search('<@264820556375916546>')||msg.search('@moeiscool')):
             if(!s.oneTimeMessages[foundTerm]){s.oneTimeMessages[foundTerm]={}}
             if(!s.oneTimeMessages[foundTerm][msg.username]){
                 s.oneTimeMessages[foundTerm][msg.username]=1
-                msg.theReply='Hi there! Shinobi CE is free, thus making it mostly "self-serve". Please review the docs http://shinobi.video/docs/. If you are looking for Moe please know that developer support provided in the Shinobi CE chat is a curteousy. If you must speak with him please consider a Support Package http://billing.place/cart/shinobi/ or Shinobi Pro http://billing.place/cart/shinobi-pro/. If you would like to make a business proposal you can start a direct message with him or fill out the contact form. http://shinobi.video/contact Thanks! :v::skin-tone-4: :sunglasses:'
+                msg.theReply='Hi there! If you are looking for Moe please know that developer support provided in the Shinobi CE chat is a curteousy. Please ask the question without "@moeiscool" so that others feel okay to answer :smile:. Before asking for help please review the docs http://shinobi.video/docs/. If you would like direct support from a developer please consider a Support Package http://billing.place/cart/shinobi/ or Shinobi Pro http://billing.place/cart/shinobi-pro/. If you would like to make a business proposal you can start a direct message with Moe or fill out the contact form. http://shinobi.video/contact Thanks! :v::skin-tone-4: :sunglasses:'
             }
         break;
     }
